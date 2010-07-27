10ee68c48e3a83cc94d531fe8701c7b7b1f0e3c9
Andreas Fredriksson <deplinenoise@gmail.com>
2010-07-27 17:12:17 +0200

    Implemented platform filter for libraries.

--- scripts/tundra/nodegen/native.lua
+++ scripts/tundra/nodegen/native.lua
@@ -2,10 +2,28 @@ local _generator = ...
 local util = require("tundra.util")
 local nodegen = require("tundra.nodegen")
 
+local function config_filter_match(pattern, build_id)
+	local p = '^' .. pattern:gsub('-', '%%-'):gsub('%*', '%%w-') .. '$'
+	local res = string.match(build_id, p)
+	--printf("matching %s with %s (from %s) => %s", p, build_id, pattern, util.tostring(res))
+	return res
+end
+
 local function install_libs(unit_env, decl)
-	for _, libname in util.nil_ipairs(decl.Libs) do
-		assert(type(libname) == "string")
-		unit_env:append("LIBS", libname)
+	for _, item in util.nil_ipairs(decl.Libs) do
+		local t = type(item) 
+		if t == "string" then
+			unit_env:append("LIBS", item)
+		elseif t == "table" then
+			local cfg_filter = item.Config
+			if config_filter_match(cfg_filter, unit_env:get("BUILD_ID")) then
+				for _, lib in ipairs(item) do
+					unit_env:append("LIBS", lib)
+				end
+			end
+		else
+			errorf("Libs must contain either strings or arrays; not %s", t)
+		end
 	end
 end
 
