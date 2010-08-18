5bf60c003f969ffdf2964f6482a6efaba50d344d
Andreas Fredriksson <dep@defmacro.se>
2010-08-18 08:28:49 +0200

    Added msvc build for hello world.

--- examples/hello-world/tundra.lua
+++ examples/hello-world/tundra.lua
@@ -6,5 +6,10 @@ Build {
 			DefaultOnHost = "macosx",
 			Tools = { "gcc", "mono" },
 		},
+		{
+			Name = "win32-msvc",
+			DefaultOnHost = "windows",
+			Tools = { "msvc-vs2008", "mono" },
+		},
 	},
 }
