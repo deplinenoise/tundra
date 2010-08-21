00d5f7fa145f68331072bd015bf53806d194d814
Andreas Fredriksson <dep@defmacro.se>
2010-08-21 18:52:21 +0200

    Added Mac OS X cross win32 compilation setup of Tundra.

--- tundra.lua
+++ tundra.lua
@@ -24,11 +24,26 @@ local common = {
 Build {
 	Units = "units.lua",
 	Configs = {
-		{ Name = "macosx-clang", Inherit = common, Tools = { "clang-osx" }, DefaultOnHost = "macosx" },
-		{ Name = "macosx-gcc", Inherit = common, Tools = { "gcc-osx" } },
-		{ Name = "win32-msvc", Inherit = common, Tools = { { "msvc-vs2008"; TargetArch = "x86"} } },
-		{ Name = "win64-msvc", Inherit = common, Tools = { { "msvc-vs2008"; TargetArch = "x64"} } },
-		{ Name = "linux-gcc", Inherit = common, Tools = { "gcc" } },
+		Config { Name = "macosx-clang", Inherit = common, Tools = { "clang-osx" }, DefaultOnHost = "macosx" },
+		Config { Name = "macosx-gcc", Inherit = common, Tools = { "gcc-osx" } },
+		Config { Name = "win32-msvc", Inherit = common, Tools = { { "msvc-vs2008"; TargetArch = "x86"} } },
+		Config { Name = "win64-msvc", Inherit = common, Tools = { { "msvc-vs2008"; TargetArch = "x64"} } },
+		Config { Name = "linux-gcc", Inherit = common, Tools = { "gcc" } },
+
+		-- MingW32 cross compilation under OS X
+		Config {
+			Name = "macosx-mingw32",
+			Inherit = common,
+			Tools = { "gcc" },
+			Env = {
+				_GCC_BINPREFIX="/usr/local/i386-mingw32-4.3.0/bin/i386-mingw32-",
+				CCOPTS = "-Werror",
+			},
+			ReplaceEnv = {
+				PROGSUFFIX=".exe",
+				SHLIBSUFFIX=".dll",
+			},
+		},
 	},
 }
 
