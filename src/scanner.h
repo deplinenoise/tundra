5c2f63bc6209668788e0f899cbe58d09bb667dd4
Andreas Fredriksson <dep@defmacro.se>
2011-11-27 09:45:07 +0100

    Factored out common scanner code into scanner.c.

--- src/scanner.h
+++ src/scanner.h
@@ -24,14 +24,24 @@ struct lua_State;
 struct td_node;
 struct td_engine;
 struct td_scanner;
+struct td_file;
+struct td_alloc;
 
 #define TUNDRA_SCANNER_REF_MTNAME "tundra_scanner_ref"
 
-typedef int (*td_scan_fn)(struct td_engine *engine, struct td_node *node, struct td_scanner *state);
+typedef struct td_include_data_tag {
+	const char *string;
+	unsigned short string_len;
+	unsigned char is_system_include;
+} td_include_data;
+
+typedef int (td_scan_fn)(struct td_alloc *scratch, const char *start, td_include_data *dest);
 
 typedef struct td_scanner {
 	const char *ident;
-	td_scan_fn scan_fn;
+	td_scan_fn *scan_fn;
+	struct td_file **include_paths;
+	int path_count;
 } td_scanner;
 
 typedef struct td_scanner_ref
@@ -44,5 +54,7 @@ td_scanner *td_alloc_scanner(struct td_engine *engine, struct lua_State* L, int 
 #define td_check_scanner(L,index) (((td_scanner_ref*) luaL_checkudata(L, index, TUNDRA_SCANNER_REF_MTNAME))->scanner)
 
 int td_scanner_open(struct lua_State *L);
+
+int td_scan_includes(struct td_engine *engine, struct td_node *node, struct td_scanner *state);
 
 #endif
