36244296277433e94f1515fcdfd28c6d5d7d9af4
Josh Simmons <simmons.44@gmail.com>
2013-04-23 15:23:24 +1000

    Cleanup startup script and TundraExePath usage

--- scripts/tundra.lua
+++ scripts/tundra.lua
@@ -3,31 +3,32 @@ require "strict"
 local os      = require "os"
 local package = require "package"
 local path    = require "tundra.path"
+local boot    = require "tundra.boot"
 
-local function main(...)
-  local cmd_args = { }
-  for i = 1, select('#', ...) do
-    cmd_args[#cmd_args + 1] = select(i, ...)
-  end
+local actions = {
+  ['generate-dag'] = function(build_script)
+    assert(build_script, "need a build script name")
+    boot.generate_dag_data(build_script)
+  end,
 
-  local action = assert(cmd_args[1], "need an action")
+  ['generate-ide-files'] = function(build_script, ide_script)
+    assert(build_script, "need a build script name")
+    assert(ide_script, "need a generator name")
+    boot.generate_ide_files(build_script, ide_script)
+  end,
 
-  local boot = require "tundra.boot"
-
-  if action == 'generate-dag' or action == 'generate-ide-files' then
-    local build_script = assert(cmd_args[2], "need a build script name")
-    if action == 'generate-ide-files' then
-      local ide_script = assert(cmd_args[3], "need a generator name")
-      boot.generate_ide_files(build_script, ide_script)
-    else
-      boot.generate_dag_data(build_script)
-    end
-  elseif action == 'selftest' then
+  ['selftest'] = function()
     require "tundra.selftest"
-  else
-    print("unknown action " .. action)
-    os.exit(1)
   end
+}
+
+local function main(action_name, ...)
+  assert(action_name, "need an action")
+
+  local action = actions[action_name]
+  assert(action, "unknown action")
+
+  action(...)
 end
 
 return {
