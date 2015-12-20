5f96c0234e5669427ac19894dfae2d41b71ab027
Andreas Fredriksson <dep@defmacro.se>
2015-12-21 00:09:40 +0100

    Rename crazy parameter name.

--- scripts/tundra/syntax/flex.lua
+++ scripts/tundra/syntax/flex.lua
@@ -19,7 +19,7 @@ DefRule {
     local src = data.Source
     local base_name = path.drop_suffix(src)
     local gen_c = data.OutputCFile or (base_name .. '.c')
-    local gen_h = data.OutputHeaderFileFile or (base_name .. '.h')
+    local gen_h = data.OutputHeaderFile or (base_name .. '.h')
     return {
       InputFiles = { src },
       OutputFiles = { gen_c, gen_h },
