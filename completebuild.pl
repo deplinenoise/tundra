bf4a8c745296ea8069bebec583d39dc37196dcbd
Lucas Meijer <lucas@unity3d.com>
2018-01-10 10:55:02 +0100

    add tundra source drop to the buildszip to comply with gpl

--- completebuild.pl
+++ completebuild.pl
@@ -1,1 +1,2 @@
+system("rsync -av .. src --exclude build --exclude artifacts --exclude .git --delete") eq 0 or die("failed creating builds.zip");
 system("zip -r builds.zip *") eq 0 or die("failed creating builds.zip");
