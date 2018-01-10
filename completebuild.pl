e0ebeccf359b6159356c0c596722f7bec5618caf
Lucas Meijer <lucas@unity3d.com>
2018-01-10 15:28:05 +0100

    make build folder

--- completebuild.pl
+++ completebuild.pl
@@ -1,2 +1,3 @@
-system("rsync -av . build/src --exclude build --exclude artifacts --exclude .git --delete") eq 0 or die("failed creating builds.zip");
+system("mkdir -p build");
+system("rsync -av . build/src --exclude build --exclude artifacts --exclude .git --delete") eq 0 or die("failed rsync");
 system("zip -r builds.zip build/*") eq 0 or die("failed creating builds.zip");
