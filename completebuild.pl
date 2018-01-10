bee0a367af1747a1912b8d2af25a159464ac4ec9
Lucas Meijer <lucas@unity3d.com>
2018-01-10 15:53:55 +0100

    try to make the archive less deep

--- completebuild.pl
+++ completebuild.pl
@@ -1,3 +1,3 @@
 system("mkdir -p build");
 system("rsync -av . build/src --exclude build --exclude artifacts --exclude .git --delete") eq 0 or die("failed rsync");
-system("zip -r builds.zip build/*") eq 0 or die("failed creating builds.zip");
+system("cd build && zip -r ../builds.zip *") eq 0 or die("failed creating builds.zip");
