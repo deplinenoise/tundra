7f373766fff2b6a88bf13f8738b3b5e41fbc93cd
Ryan J Gray <ryan@ryanjgray.com>
2022-12-17 14:42:29 +0000

    Make Tundra check for 64-bit install path.

--- scripts/tundra/tools/msvc-vscommon-next.lua
+++ scripts/tundra/tools/msvc-vscommon-next.lua
@@ -102,6 +102,10 @@ function apply_msvc_visual_studio(version, env, options)
   local platform_type = options.PlatformType --default empty ({empty} | store | uwp)
   local sdk_version = options.SdkVersion --default from vcvarsall.bat (otherwise request a specific version through this option)
   local installation_path = options.InstallationPath or "C:\\Program Files (x86)\\Microsoft Visual Studio"
+  -- VS2022 is 64-bit and gets installed into Program Files.
+  if (!options.InstallationPath and (version == "2022")) {
+    installation_path = "C:\\Program Files\\Microsoft Visual Studio"
+  }
   local vs_product = options.Product
   local vcvarsall_bat = options.VCVarsPath --override the search strategy and use a specific 'vcvars' bat file
   local search_set = {}
